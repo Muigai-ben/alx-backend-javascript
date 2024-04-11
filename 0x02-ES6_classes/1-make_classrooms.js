1dc6add11cf7cda3006e4bd0c1f76fe35f00c739
Muigai-ben <muigaibenson37@gmail.com>
2024-04-11 10:29:17 +0000

    places

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,16 @@
+// Importing the ClassRoom class
+import ClassRoom from './ClassRoom.js';
+
+// Function to initialize rooms
+function initializeRooms() {
+    // Creating an array of ClassRoom objects with the specified sizes
+    const rooms = [
+        new ClassRoom(19),
+        new ClassRoom(20),
+        new ClassRoom(34)
+    ];
+    return rooms;
+}
+
+// Exporting the initializeRooms function
+export default initializeRooms;
